268b826d51ba8228aa778b3d38d928ca78ed9493
unknown <sahindouabdallah@gmail.com>
2024-02-13 12:25:20 +0100

    frist commit

---
+++ js/misePage.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    // Fonction pour charger le contenu d'un fichier externe dans un div spécifique
+    function loadContent(pageUrl, targetDiv) {
+        $.ajax({
+            url: pageUrl,
+            dataType: 'html',
+            success: function(response) {
+                $(targetDiv).html(response);
+            },
+            error: function(xhr, status, error) {
+                console.error('Erreur lors du chargement du contenu: ' + error);
+            }
+        });
+    }
+
+    // Écouteurs d'événements pour les boutons
+    $('#btnAbout').click(function(){
+        loadContent('template/about.html', '#content'); // Charge le contenu de about.html dans #content
+    });
+
+    $('#btnContact').click(function(){
+        loadContent('contact.html', '#content'); // Charge le contenu de contact.html dans #content
+    });
+});
